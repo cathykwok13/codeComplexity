8e5b4877add58a36b2f60a31c8dd05923ca2e231
cathykwok13 <cathy_kwok@yahoo.ca>
2013-10-31 22:42:28 -0700

    add new classes

---
+++ codeComplexity/src/proj/tools/complexity/CyclomaticCalculator.java
@@ -0,0 +1,13 @@
+package proj.tools.complexity;
+
+/**
+ * The purpose of this class is to calculate the 
+ * code cyclomatic complexity of a method
+ * @author cathy
+ *
+ */
+
+
+public class CyclomaticCalculator {
+
+}
