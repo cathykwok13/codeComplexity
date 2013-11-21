e4690ce1e9595b95ff2a721cde6721c61ffd6651
cathyk13 <cathy_kwok@yahoo.ca>
2013-11-21 12:44:54 -0800

    done! calculates exit nodes and decision points

--- codeComplexity/src/proj/tools/complexity/CyclomaticCalculator.java
+++ codeComplexity/src/proj/tools/complexity/CyclomaticCalculator.java
@@ -4,63 +4,56 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
- * The purpose of this class is to calculate the 
- * code cyclomatic complexity of a method
+ * The purpose of this class is to calculate the code cyclomatic complexity of a
+ * method
  * 
- * The complexity M is then defined as
- * 		M = E − N + 2P,
- * where
- * 		E = the number of edges of the graph,
- * 		N = the number of nodes of the graph,
- * 		P = the number of connected components (exit nodes). 
+ * The complexity M is then defined as M = pi - s + 2
  * 
- * It can be shown that the cyclomatic complexity of any structured program with only one entrance point 
- * and one exit point is equal to the number of decision points (i.e., "if" statements or conditional loops) 
- * contained in that program plus one.
+ * Where pi is the number of decision points (case,if,for) s is the number of
+ * exit points
+ * 
  * @author cathy
- *
+ * 
  */
 
-
 public class CyclomaticCalculator {
-	
-	
-	static int getCyclomaticCalc(String s){
-		
-		//calculate how many exit nodes
+
+	static int getCyclomaticCalc(String s) {
+		return getDecisionPoint(s) - getExitPoints(s) + 2;
+	}
+
+	private static int getDecisionPoint(String s) {
+
+		Pattern ifP = Pattern.compile("if\\s?\\(");
+		Pattern caseP = Pattern.compile("case\\s{1}[A-Za-z]+\\s?:");
+		Pattern forP = Pattern.compile("for\\s?\\(");
+		Pattern whileP = Pattern.compile("while\\s?\\(");
+		Matcher ifMatch = ifP.matcher(s);
+		Matcher caseMatch = caseP.matcher(s);
+		Matcher forMatch = forP.matcher(s);
+		Matcher whileMatch = whileP.matcher(s);
+
+		int count = 0;
+
+		while (ifMatch.find() || caseMatch.find() || forMatch.find()
+				|| whileMatch.find())
+			count++;
+
+		return count;
+	}
+
+	private static int getExitPoints(String s) {
+		// calculate how many exit nodes
 		Pattern p = Pattern.compile("return");
 		Matcher matcher = p.matcher(s);
 
 		int count = 0;
-		while(matcher.find())
+		while (matcher.find())
 			count++;
-		
-		if(count>1){
-			//do the algo with >1 exit node
-			return 0;
-		}else{
-			return getDecisionPoint(s) + 1;
-		}
-		
-	}
-	
-	private static int getDecisionPoint(String s){
-		
 
-		Pattern ifP = Pattern.compile("if\\s?\\(");
-		Pattern caseP = Pattern.compile("case\\s{1}[A-Za-z]+\\s?:");
-		Matcher ifMatch = ifP.matcher(s);
-		Matcher caseMatch = caseP.matcher(s);
-
-		int count = 0;
-		while(ifMatch.find())
-			count++;
-		while(caseMatch.find())
-			count++;
-	
-		//System.out.println(count);		
+		if (count == 0)
+			return 1;
 		return count;
 	}
-	
 
 }
