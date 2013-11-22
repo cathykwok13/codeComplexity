d09f8c37006d176b4d955ab2e692bc43004ee105
cathyk13 <cathy_kwok@yahoo.ca>
2013-11-21 21:47:43 -0800

    dir parser to parse through bug folders

--- codeComplexity/src/proj/tools/complexity/Analyzer.java
+++ codeComplexity/src/proj/tools/complexity/Analyzer.java
@@ -1,5 +1,6 @@
 package proj.tools.complexity;
 
+import java.io.FileNotFoundException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -12,7 +13,7 @@ public class Analyzer {
 
 	public static void main(String[] args) {
 
-		String path = "/Users/cathykwok/Documents/School/CPSC410/GoldenApple/";
+		String path = "/Users/cathykwok/Documents/School/CPSC410/";
 		analyze(path);
 	}
 
@@ -23,65 +24,78 @@ public class Analyzer {
 	 */
 	private static void analyze(String path) {
 
-		File bugReport = new File("/Users/selenalee/Documents/Cpsc 410/Project/Bug Report.txt");
-		BugReportParser bugParser = new BugReportParser();
-		String[] commitId = bugParser.readCommitId(bugReport);
-		
-		// get all java files
-		
-		DirectoryParser dirParser = new DirectoryParser(path);
-		List<String> javaFilePaths = dirParser.getJavaFiles();
-		List<String> javaClassList = dirParser.getClassList();
-		ClassMetrics[] projectMetrics = new ClassMetrics[javaFilePaths.size()];
-		ClassParser claParser = new ClassParser();
-		
-		
-		// create a classMetrics object for each class
+		DirectoryParser projectParser = new DirectoryParser(path);
+		List<String> commitIdPath = null;
+		try {
+			commitIdPath = projectParser.getFolders();
+		} catch (FileNotFoundException e) {
+			System.out.println("FILE PATH ERROR");
+			e.printStackTrace();
+		}
 
-		for (int i = 0; i < javaFilePaths.size(); i++) {
+		// FOR EACH PROJECt
 
-			// values required for ClassMetrics
-			int linesOfCode = 0;
-			String commitId = "";
-			Map<String, Integer> complexityPerMethod = new HashMap<String, Integer>();
-			Map<String, Integer> dependencyPerMethod = new HashMap<String, Integer>();
+		for (int k = 0; k < commitIdPath.size(); k++) {
+			// get all java files
+			DirectoryParser dirParser = new DirectoryParser(commitIdPath.get(k));
+			List<String> javaFilePaths = dirParser.getJavaFiles();
 
-			// get all the methods for the class
-			claParser.setFilePath(javaFilePaths.get(i));
-			List<String> methodList = claParser.getMethodList();
-			
+			List<String> javaClassList = dirParser.getClassList();
+			ClassMetrics[] projectMetrics = new ClassMetrics[javaFilePaths
+					.size()];
+			ClassParser claParser = new ClassParser();
 
-			// TODO
-			// calculate the cyclomatic complexity for this class
-			for (int j = 0; j < methodList.size(); j++) {
-				// removes class constructors
-				if (methodList.get(j).length() > 0
-						&& (methodList.get(j).contains("void") || methodList
-								.get(j).contains("return"))) {
-					int complexity = CyclomaticCalculator
-							.getCyclomaticCalc(methodList.get(j));
+			// create a classMetrics object for each class
 
-					complexityPerMethod.put(javaClassList.get(i)+j, complexity);
-					
-					 int dependency =
-					 CouplingCalculator.getCouplingCalc(methodList.get(j), javaClassList);
-					 dependencyPerMethod.put(javaClassList.get(i)+j,dependency);
+			for (int i = 0; i < javaFilePaths.size(); i++) {
+
+				// values required for ClassMetrics
+				int linesOfCode = 0;
+				String commitId = "";
+				Map<String, Integer> complexityPerMethod = new HashMap<String, Integer>();
+				Map<String, Integer> dependencyPerMethod = new HashMap<String, Integer>();
+
+				// get all the methods for the class
+				claParser.setFilePath(javaFilePaths.get(i));
+				List<String> methodList = claParser.getMethodList();
+
+				// TODO
+				// calculate the cyclomatic complexity for this class
+				for (int j = 0; j < methodList.size(); j++) {
+					// removes class constructors
+					if (methodList.get(j).length() > 0
+							&& (methodList.get(j).contains("void") || methodList
+									.get(j).contains("return"))) {
+						int complexity = CyclomaticCalculator
+								.getCyclomaticCalc(methodList.get(j));
+
+						complexityPerMethod.put(javaClassList.get(i) + j,
+								complexity);
+
+						int dependency = CouplingCalculator.getCouplingCalc(
+								methodList.get(j), javaClassList);
+						dependencyPerMethod.put(javaClassList.get(i) + j,
+								dependency);
+					}
 				}
+
+				// TODO fill in values of class metrics
+				projectMetrics[i] = new ClassMetrics(linesOfCode, commitId,
+						complexityPerMethod, dependencyPerMethod);
+
+//				System.out.println("CLASS ***** " + javaClassList.get(i));
+//				System.out.println("Lines Of Code: "
+//						+ projectMetrics[i].getLinesOfCode());
+//				System.out.println("Commit Id: "
+//						+ projectMetrics[i].getCommitId());
+//				System.out.println("Cyclomatic Complexity: "
+//						+ projectMetrics[i].getComplexityPerMethod());
+//				System.out.println("Coupling : "
+//						+ projectMetrics[i].getDependencyPerMethod());
+
 			}
 
-			// TODO fill in values of class metrics
-			projectMetrics[i] = new ClassMetrics(linesOfCode, commitId,
-					complexityPerMethod, dependencyPerMethod);
-			
-			System.out.println("CLASS ***** "+ javaClassList.get(i));
-			System.out.println("Lines Of Code: "+projectMetrics[i].getLinesOfCode());
-			System.out.println("Commit Id: "+projectMetrics[i].getCommitId());
-			System.out.println("Cyclomatic Complexity: "+projectMetrics[i].getComplexityPerMethod());
-			System.out.println("Coupling : "+projectMetrics[i].getDependencyPerMethod());
-
 		}
-		
-		
 
 	}
 
--- codeComplexity/src/proj/tools/parser/DirectoryParser.java
+++ codeComplexity/src/proj/tools/parser/DirectoryParser.java
@@ -1,35 +1,64 @@
 package proj.tools.parser;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
- * DirectoryParser takes a directory path and parses through
- * the directories to get all the paths to the java files 
+ * DirectoryParser takes a directory path and parses through the directories to
+ * get all the paths to the java files
+ * 
  * @author cathy
- *
+ * 
  */
 public class DirectoryParser {
 
 	File[] projDirContents;
 	List<String> javaList = new ArrayList<String>();
 	List<String> classList = new ArrayList<String>();
-
+	
 	public DirectoryParser(String path) {
 		projDirContents = new File(path).listFiles();
 	}
 
+	public List<String> getFolders() throws FileNotFoundException {
+		File bugReport = new File(
+				"/Users/cathykwok/Documents/School/CPSC410/bugReport.txt");
+		String[] commitId = BugReportParser.readCommitId(bugReport);
+		
+		String[] folderNamesInArray = new String[commitId.length];
+
+		for (int i = 0; i < projDirContents.length; i++) {
+			int index = getCommitIndex(commitId, projDirContents[i].getName());
+			if (projDirContents[i].isDirectory() && index >= 0 ) {
+				folderNamesInArray[index] = projDirContents[i].getPath();
+				System.out.println(projDirContents[i].getName());
+			}
+		}
+		//folderNames = 
+		return Arrays.asList(folderNamesInArray);
+	}
+
+	private int getCommitIndex(String[] commitId, String name ){
+		for(int i = 0; i<commitId.length ; i++){
+			if(name.contains(commitId[i].trim()))
+				return i;
+		}
+		return -1;
+	}
+	
 	public List<String> getJavaFiles() {
 		getJavaFiles(projDirContents);
 		return javaList;
 	}
-	
+
 	public List<String> getClassList() {
 		getJavaFiles(projDirContents);
 		return classList;
 	}
-	
+
 	private void getJavaFiles(File[] files) {
 		for (File file : files) {
 			if (file.isDirectory()) {
