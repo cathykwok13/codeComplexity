ce7b7abc385b931b118de58c7e38e43a5e48df36
selenalee <selenalee@dhcp-128-189-229-42.ubcsecure.wireless.ubc.ca>
2013-11-21 19:02:01 -0800

    Cleaned out bugreport code in DirectoryParser

--- codeComplexity/src/proj/tools/parser/DirectoryParser.java
+++ codeComplexity/src/proj/tools/parser/DirectoryParser.java
@@ -15,26 +15,11 @@ public class DirectoryParser {
 	File[] projDirContents;
 	List<String> javaList = new ArrayList<String>();
 	List<String> classList = new ArrayList<String>();
-	
-	File bugReport = new File("/Users/selenalee/Documents/Cpsc 410/Project/Bug Report.txt");
-	BugReportParser bugParser = new BugReportParser();
-	String[] commitId = bugParser.readCommitId(bugReport);
-	
+
 	public DirectoryParser(String path) {
 		projDirContents = new File(path).listFiles();
 	}
 
-	public void orderFiles() {
-		File[] dirContents = new File(path).listFiles();
-		for (int i=0; i<commitId.length; i++) {
-			if (dirConetents.getName().contains(commitId[i])) {
-				projDirContents[i] = dirContents[i];
-			} else {
-				
-			}
-		}
-	}
-	
 	public List<String> getJavaFiles() {
 		getJavaFiles(projDirContents);
 		return javaList;
